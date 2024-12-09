cc6a715b546bd79be7ecc946ffbd2197983b6cbc
Martín Pravia <marto.pravia@gmail.com>
2024-12-09 09:25:20 +0000

    creo estructura y realizo fetchapi, a su vez le indico en el map dentro de ul que mapee cada task y me la muestre con el task.id y task.label

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const [tasks, setTasks] = useState([]);
+const api = "https://playground.4geeks.com/todo"
+const lastApi = "/users/martopravia"
+const myApi = `${api}${lastApi}`
+
+useEffect(() => {
+    const fetchApi = async () => {
+        try {
+            const response = await fetch(myApi);
+            if (!response.ok) {
+                throw new Error("Error al obtener los datos de la API");
+            }
+            const data = await response.json();
+            console.log("Datos obtenidos:", data);
+            setTasks(data.todos);
+        } catch (error) {
+            console.error("Error al realizar el fetch:", error);
+        }
+    };
+    fetchApi(); 
+}, []);
+
+  return (
+	<>
+	<div className="row d-flex justify-content-center">
+	  <div className="col-8">
+		<label className="form-label d-flex justify-content-center align-items-center">
+		  <h1 className="display-3 title"> To Do List</h1>
+		</label>
+		<input
+		  type="text"
+		  className="form-control"
+            id="inputText"
+            aria-describedby="InputText"
+            placeholder="Insert here your item to the list"
+		  />
+		   <ul className="list-group">
+            {tasks.map((task) => {
+              return (
+                <li
+                  className="list-group-item d-flex justify-content-between"
+                  key={task.id}
+                >
+                  {task.label}
+                  <i className="bi bi-trash trash " ></i>
+                </li>
+              );
+            })}
+          </ul>
+		   </div>
+		   </div>
+		  </>
+  )
 };
 
 export default Home;
